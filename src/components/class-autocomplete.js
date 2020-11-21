13394e77f164e689b565e4de0689049089dab59d
Karol Jakubcewicz <karol.jakubcewicz@gmail.com>
2020-11-21 19:43:18 +0100

    Add controlled text input field

--- src/components/class-autocomplete.js
+++ src/components/class-autocomplete.js
@@ -1,11 +1,36 @@
 import React from "react";
+import "./class-autocomplete.css";
 
 export class ClassAutocomplete extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      searchQuery: "",
+    };
+  }
+
+  handleChange(event) {
+    const query = event.target.value;
+
+    this.setState({ searchQuery: query });
   }
 
   render() {
-    return <>Hello world</>;
+    const { searchQuery } = this.state;
+
+    return (
+      <div className="class-autocomplete--wrapper">
+        <label htmlFor="customer-class-autocomplete">
+          Search customers by name
+        </label>
+        <input
+          id="customer-class-autocomplete"
+          placeholder="Search customers"
+          onChange={(event) => this.handleChange(event)}
+          value={searchQuery}
+        />
+      </div>
+    );
   }
 }
